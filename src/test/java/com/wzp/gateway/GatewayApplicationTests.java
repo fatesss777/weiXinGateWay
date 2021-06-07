108a012e4ff2b673820a975cfa9817ea3a9f6488
fatesss777 <15754518018@163.com>
2021-06-07 18:24:26 +0800

    网关

---
+++ src/test/java/com/wzp/gateway/GatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wzp.gateway;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
